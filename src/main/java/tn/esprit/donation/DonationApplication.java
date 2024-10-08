d8dff5b9b3a19380ccacd57d4fc46f9ae3800c35
omar essid <Omar.Essid@esprit.tn>
2024-10-09 00:27:39 +0100

    update src

--- src/main/java/tn/esprit/donation/DonationApplication.java
+++ src/main/java/tn/esprit/donation/DonationApplication.java
@@ -2,16 +2,10 @@ package tn.esprit.donation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
-@EnableAspectJAutoProxy
 @SpringBootApplication
 public class DonationApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DonationApplication.class, args);
     }
-
 }
